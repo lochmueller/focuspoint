c5f781761a4ba1e33e566848d7d1eb4d22b2cb01
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-05-22 09:47:13 +0200

    Change TYPO3 dependency to 6.2.x-7.2.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company'   => 'hdnet.de',
 	'constraints'      => array(
 		'depends' => array(
-			'typo3'      => '6.2.0-7.1.99',
+			'typo3'      => '6.2.0-7.2.99',
 			'autoloader' => '1.5.6-1.6.0',
 		),
 	),
