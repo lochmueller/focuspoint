a72d43a638e4ab11624cd95c087032a5816c5f27
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-08-31 16:49:18 +0200

    Cleanup module registration

--- ext_tables.php
+++ ext_tables.php
@@ -9,10 +9,7 @@ if (!defined('TYPO3_MODE')) {
 $icons = array(
     'focuspoint' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('focuspoint') . 'ext_icon.png',
 );
-// Gives the $icon array to the sprite manager
 \TYPO3\CMS\Backend\Sprite\SpriteManager::addSingleIcons($icons, 'focuspoint');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath('focuspoint',
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Modules/Wizards/Focuspoint/');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath('focuspoint', 'EXT:focuspoint/Modules/Wizards/Focuspoint/');
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['focuspoint_test'] = 'image,image_ratio';
