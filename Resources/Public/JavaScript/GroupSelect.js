c2fc84906fe9f9cba70b84decb399a2ec148d0e6
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-11-04 16:58:51 +0100

    Respect select boxes with a size of <= 1

--- Resources/Public/JavaScript/GroupSelect.js
+++ Resources/Public/JavaScript/GroupSelect.js
@@ -3,15 +3,23 @@ define('TYPO3/CMS/Focuspoint/GroupSelect', ['jquery'], function ($) {
 
         $('a.group-focuspoint').click(function (e) {
             e.preventDefault();
-
-            var selection = $(this).parentsUntil('.t3-form-field-item').find('select').val();
-            if (selection === null || selection.length != 1) {
+            var selectBox = $(this).parentsUntil('.t3-form-field-item').find('select');
+            var selection = selectBox.val();
+            if (selection === null) {
                 alert('You have to selection one single image in the list of images!');
                 return;
             }
 
-            var image = selection.join();
-            document.location.href = $(this).attr('href') + '&P[file]=' + image;
+            if (parseInt(selectBox.attr('size'), 10) > 1) {
+                // multiselect
+                if (selection.length > 1) {
+                    alert('You have to selection one single image in the list of images!');
+                    return;
+                }
+                selection = selection.join();
+            }
+
+            document.location.href = $(this).attr('href') + '&P[file]=' + selection;
         });
     });
 });
