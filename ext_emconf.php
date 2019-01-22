92c7a84605183c7642b1c9b6e44f887f70b75445
Tim Lochmüller <tim.lochmueller@hdnet.de>
2019-01-22 13:39:55 +0100

    Set the right versions for dependecnies

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.0.0-0.0.0',
-            'typo3' => '8.7.0-9.0.99',
+            'typo3' => '8.7.0-9.5.99',
             'autoloader' => '4.0.0-0.0.0',
         ],
     ],
