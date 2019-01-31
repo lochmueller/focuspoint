b87c445fe5b22003a225d0fe47c9487cf3321807
Tim Lochmüller <tim@fruit-lab.de>
2019-01-31 21:20:03 +0100

    Fix #64 - take care that getCurrentRatioConfiguration is always string or a exception is thrown

--- Classes/Xclass/LocalCropScaleMaskHelper.php
+++ Classes/Xclass/LocalCropScaleMaskHelper.php
@@ -90,6 +90,7 @@ class LocalCropScaleMaskHelper extends \TYPO3\CMS\Core\Resource\Processing\Local
                 ObjectAccess::setProperty($task, 'targetFile', $targetFile, true);
             }
         } catch (\Exception $ex) {
+            // not handled
         }
         self::$deepCheck = false;
 
@@ -99,20 +100,24 @@ class LocalCropScaleMaskHelper extends \TYPO3\CMS\Core\Resource\Processing\Local
     /**
      * Find the current ratio configuration.
      *
-     * @return string|null
+     * @return string
+     * @throws \Exception
      */
-    protected function getCurrentRatioConfiguration()
+    protected function getCurrentRatioConfiguration(): string
     {
         $currentRecord = $GLOBALS['TSFE']->currentRecord;
         $parts = GeneralUtility::trimExplode(':', $currentRecord);
         if (2 !== sizeof($parts)) {
-            return null;
+            throw new \Exception('Invalid count of current record parts', 12367);
         }
         if ('tt_content' !== $parts[0]) {
-            return null;
+            throw new \Exception('Invalid part 0. part 0 have to be tt_content', 127383);
         }
         $record = BackendUtility::getRecord($parts[0], (int) $parts[1]);
+        if (!isset($record['image_ratio'])) {
+            throw new \Exception('No image_ratio found in the current record', 324672);
+        }
 
-        return isset($record['image_ratio']) ? trim($record['image_ratio']) : null;
+        return trim((string) $record['image_ratio']);
     }
 }
