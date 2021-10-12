6bb3b470599992fb258385841ebf50bb4f33c5ab
Tim <tim@fruit-lab.de>
2021-10-12 22:58:40 +0200

    Start dev for v11 release

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,8 +22,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'hdnet.de',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-7.4.99',
-            'typo3' => '10.0.0-10.5.99',
+            'php' => '7.3.0-8.0.99',
+            'typo3' => '10.0.0-11.5.99',
             'autoloader' => '7.0.0-7.99.0',
         ],
     ],
