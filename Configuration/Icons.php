001939aa437746a175f445d13d1ddab612b367ac
Tim Lochmüller <tim@fruit-lab.de>
2023-05-14 19:10:00 +0200

    Move icon registration to Configuration folder

---
+++ Configuration/Icons.php
@@ -0,0 +1,14 @@
+<?php
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'tcarecords-tx_focuspoint_domain_model_filestandalone-default' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:focuspoint/Resources/Public/Icons/Extension.svg',
+    ],
+    'tcarecords-tx_focuspoint_domain_model_filestandalone-default-light' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:focuspoint/Resources/Public/Icons/ExtensionLight.svg',
+    ],
+];
--- ext_tables.php
+++ ext_tables.php
@@ -12,21 +12,4 @@ $loader = [
 ];
 \HDNET\Autoloader\Loader::extTables('HDNET', 'focuspoint', $loader);
 
-/** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
-    'tcarecords-tx_focuspoint_domain_model_filestandalone-default',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    [
-        'source' => 'EXT:focuspoint/Resources/Public/Icons/Extension.svg'
-    ]
-);
-$iconRegistry->registerIcon(
-    'tcarecords-tx_focuspoint_domain_model_filestandalone-default-light',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    [
-        'source' => 'EXT:focuspoint/Resources/Public/Icons/ExtensionLight.svg'
-    ]
-);
-
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['focuspoint_test'] = 'image,image_ratio';
